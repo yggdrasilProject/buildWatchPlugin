7679904acf5897ba573fad755bd39629c560ad1c
Alexey Deryugin <aderyugin@mirantis.com>
2016-09-20 17:02:29 +0300

    Initial implementation
    This patch adds plugin skeleton and some basic functionality.
    
     - BuildWatch task: monitor failed jobs
     - BuildProcessor service: process failed jobs results
    
    Now it supports only NoOp test jobs.
    
    Change-Id: Ief27c153032012324d801b05f4214b7a55e474d8

---
+++ src/main/java/ru/linachan/monitor/common/BuildWatchShell.java
@@ -0,0 +1,131 @@
+package ru.linachan.monitor.common;
+
+import com.mongodb.client.MongoCollection;
+import org.bson.Document;
+import org.bson.types.ObjectId;
+import ru.linachan.monitor.BuildWatchPlugin;
+import ru.linachan.monitor.common.noop.NoOpTest;
+import ru.linachan.monitor.common.noop.NoOpTestResult;
+import ru.linachan.monitor.common.noop.NoOpTestState;
+import ru.linachan.yggdrasil.YggdrasilCore;
+import ru.linachan.yggdrasil.common.console.tables.Table;
+import ru.linachan.yggdrasil.plugin.YggdrasilPluginManager;
+import ru.linachan.yggdrasil.shell.YggdrasilShellCommand;
+import ru.linachan.yggdrasil.shell.helpers.CommandAction;
+import ru.linachan.yggdrasil.shell.helpers.ShellCommand;
+
+import java.io.IOException;
+import java.util.List;
+
+@ShellCommand(command = "watch", description = "Show and manage BuildWatch reports.")
+public class BuildWatchShell extends YggdrasilShellCommand {
+
+    private MongoCollection<Document> builds;
+
+    @Override
+    protected void init() throws IOException {
+        builds = YggdrasilCore.INSTANCE
+            .getManager(YggdrasilPluginManager.class)
+            .get(BuildWatchPlugin.class)
+            .getDbClient()
+            .getCollection("builds");
+    }
+
+    private String stripLine(String line, Integer length) {
+        if (line.length() > length) {
+            return line.substring(0, length - 3) + "...";
+        }
+
+        return line;
+    }
+
+    @CommandAction("List reports")
+    public void list() throws IOException {
+        Table reports = new Table("ID", "Job", "Build ID", "Deployment Failures", "NoOp Test Failures");
+        Integer resultLimit = Integer.parseInt(kwargs.getOrDefault("limit", "15"));
+
+        for (Document report: builds.find().limit(resultLimit)) {
+            reports.addRow(
+                report.getObjectId("_id").toHexString(),
+                report.getString("buildName").split(" #")[0],
+                report.getString("buildName").split(" #")[1],
+                String.valueOf(((List) report.get("errors")).size()),
+                String.valueOf(((List) report.get("noop")).size())
+            );
+        }
+
+        console.writeTable(reports);
+    }
+
+    @SuppressWarnings("unchecked")
+    @CommandAction("Show detailed report")
+    public void show() throws IOException {
+        String reportId = args.get(0);
+
+        Document report = builds.find(new Document("_id", new ObjectId(reportId))).first();
+        if (report != null) {
+            List<String> errors = (List<String>) report.get("errors");
+            List<Document> noOpTestResults = (List<Document>) report.get("noop");
+
+            if (errors.size() > 0) {
+                Table deploymentErrors = new Table("Deployment Error");
+                for (String error: errors) {
+                    for (String errorLine: error.split("\\n")) {
+                        deploymentErrors.addRow(errorLine);
+                    }
+                }
+                console.writeTable(deploymentErrors);
+            }
+
+            if (noOpTestResults.size() > 0) {
+                Table noOpTests = new Table("Task", "YAML", "OS", "Tests Failed");
+                for (Document noOpTestResult: noOpTestResults) {
+                    NoOpTest noOpTest = NoOpTest.fromBSON(noOpTestResult);
+                    if (noOpTest.getState().equals(NoOpTestState.FAILED)) {
+                        noOpTests.addRow(
+                            noOpTest.getTaskName(),
+                            noOpTest.getConfigurationName(),
+                            noOpTest.getOs(),
+                            String.valueOf(noOpTest.getFailedTests().size())
+                        );
+                    }
+                }
+                console.writeTable(noOpTests);
+
+                if (kwargs.containsKey("noop-details")) {
+                    Table noOpTestDetails = new Table("Task", "YAML", "OS", "Test", "Result", "Reason");
+                    for (Document noOpTestResult: noOpTestResults) {
+                        NoOpTest noOpTest = NoOpTest.fromBSON(noOpTestResult);
+                        if (noOpTest.getState().equals(NoOpTestState.FAILED)) {
+                            for (NoOpTestResult testResult: noOpTest.getTests()) {
+                                String testName = testResult.getTestName();
+                                String failureReason = testResult.getFailureReason();
+
+                                if (!kwargs.containsKey("no-strip")) {
+                                    testName = stripLine(testName, 50);
+                                    failureReason = stripLine(failureReason, 50);
+                                }
+
+                                noOpTestDetails.addRow(
+                                    noOpTest.getTaskName(),
+                                    noOpTest.getConfigurationName(),
+                                    noOpTest.getOs(), testName,
+                                    testResult.getState().name(),
+                                    failureReason
+                                );
+                            }
+                        }
+                    }
+                    console.writeTable(noOpTestDetails);
+                }
+            }
+        } else {
+            console.writeLine("Report %s doesn't exists", reportId);
+        }
+    }
+
+    @Override
+    protected void onInterrupt() {
+
+    }
+}
---
+++ src/main/java/ru/linachan/monitor/jenkins/BuildProcessor.java
@@ -0,0 +1,175 @@
+package ru.linachan.monitor.jenkins;
+
+import com.mongodb.client.MongoCollection;
+import com.offbytwo.jenkins.model.BuildWithDetails;
+import org.bson.Document;
+import ru.linachan.monitor.BuildWatchPlugin;
+import ru.linachan.monitor.common.noop.NoOpTest;
+import ru.linachan.yggdrasil.YggdrasilCore;
+import ru.linachan.yggdrasil.common.Queue;
+import ru.linachan.yggdrasil.plugin.YggdrasilPluginManager;
+import ru.linachan.yggdrasil.service.YggdrasilService;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+public class BuildProcessor implements YggdrasilService {
+
+    private Queue<BuildWithDetails> failedBuilds;
+    private MongoCollection<Document> builds;
+
+    private static final Pattern FAILED_NOOP_TASK = Pattern.compile(
+        "^(?m)(?<state>FAILED|SUCCESS)\\s+(?<task>[^\\s]+)\\s+(?<os>[^\\s]+)\\s+(?<yaml>[^\\s]+)\\s*$"
+    );
+    private static final Pattern FAILED_NOOP_TEST = Pattern.compile(
+        "^(?m)\\s*(?<state>failed|success)\\s*(?<test>should .*?)$"
+    );
+    private static final Pattern ASSERTION_ERROR = Pattern.compile(
+        "^(?m)AssertionError:\\s*(?<error>.*?)\\s*$"
+    );
+    private static final Pattern FAILED_OSTF_TEST = Pattern.compile(
+        "^(?m)\\s+-\\s+(?<test>.*?)\\s*$"
+    );
+
+    private boolean isRunning = true;
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public void onInit() {
+        failedBuilds = (Queue<BuildWithDetails>) YggdrasilCore.INSTANCE.getQueue("failedBuilds");
+
+        builds = YggdrasilCore.INSTANCE
+            .getManager(YggdrasilPluginManager.class)
+            .get(BuildWatchPlugin.class)
+            .getDbClient()
+            .getCollection("builds");
+    }
+
+    @Override
+    public void onShutdown() {
+        isRunning = false;
+    }
+
+    private List<NoOpTest> findNoOpTests(String[] log) {
+        List<NoOpTest> noOpTests = new ArrayList<>();
+
+        for (int lineNo = 0; lineNo < log.length; lineNo++) {
+            Matcher failureMatcher = FAILED_NOOP_TASK.matcher(log[lineNo]);
+            if (failureMatcher.matches()) {
+                NoOpTest noOpTest = new NoOpTest(
+                    failureMatcher.group("task"),
+                    failureMatcher.group("yaml"),
+                    failureMatcher.group("os"),
+                    failureMatcher.group("state")
+                );
+
+                while (lineNo < log.length - 1) {
+                    Matcher testMatcher = FAILED_NOOP_TEST.matcher(log[lineNo + 1]);
+
+                    if (testMatcher.matches()) {
+                        noOpTest.addTest(
+                            testMatcher.group("test"),
+                            testMatcher.group("state")
+                        );
+
+                        lineNo++;
+                    } else {
+                        break;
+                    }
+                }
+
+                if (noOpTest.getTests().size() > 0) {
+                    noOpTests.add(noOpTest);
+                }
+            }
+        }
+
+        return noOpTests;
+    }
+
+    private List<String> findDeploymentErrors(String[] log) {
+        List<String> deploymentErrors = new ArrayList<>();
+
+        for (int lineNo = 0; lineNo < log.length; lineNo++) {
+            Matcher failureMatcher = ASSERTION_ERROR.matcher(log[lineNo]);
+            if (failureMatcher.matches()) {
+                String error = failureMatcher.group("error");
+
+                while (lineNo < log.length - 1) {
+                    Matcher testMatcher = FAILED_OSTF_TEST.matcher(log[lineNo + 1]);
+
+                    if (testMatcher.matches()) {
+                        error += "\n - " + testMatcher.group("test");
+                        lineNo++;
+                    } else {
+                        break;
+                    }
+                }
+
+                if (!deploymentErrors.contains(error)) {
+                    deploymentErrors.add(error);
+                }
+            }
+        }
+
+        return deploymentErrors;
+    }
+
+    @Override
+    public void run() {
+        while (isRunning) {
+            BuildWithDetails build = failedBuilds.pop();
+            if (build != null) {
+                boolean buildFound = builds.count(
+                    new Document("buildName", build.getFullDisplayName())
+                ) > 0;
+
+                if (!buildFound) {
+                    logger.info("New failed build found: {}", build.getId());
+                    try {
+                        String[] lines = build.getConsoleOutputText().split("\\r\\n");
+
+                        Document failedBuildDetails = new Document("buildName", build.getFullDisplayName())
+                            .append("description", build.getDescription())
+                            .append("noop", new ArrayList<Document>())
+                            .append("errors", new ArrayList<Document>());
+
+                        builds.insertOne(failedBuildDetails);
+
+                        List<Document> noOpTests = findNoOpTests(lines).stream()
+                            .map(NoOpTest::toBSON)
+                            .collect(Collectors.toList());
+
+                        if (noOpTests.size() > 0) {
+                            builds.updateOne(new Document("buildName", build.getFullDisplayName()), new Document(
+                                "$push", new Document(
+                                    "noop", new Document(
+                                        "$each", noOpTests
+                                    )
+                                )
+                            ));
+                        }
+
+                        List<String> deploymentErrors = findDeploymentErrors(lines);
+
+                        if (deploymentErrors.size() > 0) {
+                            builds.updateOne(new Document("buildName", build.getFullDisplayName()), new Document(
+                                "$push", new Document(
+                                    "errors", new Document(
+                                        "$each", deploymentErrors
+                                    )
+                                )
+                            ));
+                        }
+                    } catch (IOException e) {
+                        logger.error("Unable to get Build details: {}", e.getMessage());
+                    }
+                }
+            }
+        }
+    }
+}
